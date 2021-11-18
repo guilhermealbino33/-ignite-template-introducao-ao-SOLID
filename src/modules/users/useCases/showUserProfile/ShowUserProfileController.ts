79f3b19173cffb5baee55e2b9a29fb39106dffce
Guilherme Albino <guilhermealbino33@gmail.com>
2021-11-18 14:07:42 -0300

    Commit

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,14 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const id = request.body;
+      const user = this.showUserProfileUseCase.execute(id);
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,8 +6,16 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+      const userAdmin = this.turnUserAdminUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.json(userAdmin);
+    } catch (err) {
+      return response.status(404).json({ error: err });
+    }
   }
 }
-
 export { TurnUserAdminController };
